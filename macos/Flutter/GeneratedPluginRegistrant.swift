e3868e5d3c03d1766ab9575263d3d3644e607d9d
tasnuva-2023 <133502709+tasnuva-2023@users.noreply.github.com>
2025-08-12 07:38:14 +0600

    Add image_picker and other dependencies, fix pubspec duplicates

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -1,10 +1,10 @@
 //
 //  Generated file. Do not edit.
 //
+
 import FlutterMacOS
 import Foundation
 
-import file_selector_macos
 import app_links
 import file_selector_macos
 import path_provider_foundation
@@ -13,7 +13,6 @@ import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
@@ -21,4 +20,3 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
-
